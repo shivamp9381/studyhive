da1f167f555879a73872ab64ed14dd4c43058d6d
shivamp9381 <shivamp9381@gmail.com>
2024-12-29 21:31:50 +0530

    Project completed

--- base/models.py
+++ base/models.py
@@ -1,10 +1,38 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_superuser(self, email, password=None, **extra_fields):
+        """
+        Create and return a superuser with an email and password.
+        """
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractUser):
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True, null=True)
+    bio = models.TextField(null=True)
+    avatar = models.ImageField(null=True, default="avatar.svg")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']  # add 'username' here if you want it required for other operations
+
+    objects = CustomUserManager()  # Link the custom manager here
+
+
 
 class Topic(models.Model):
     name = models.CharField(max_length=200)
+
     def __str__(self):
         return self.name
 
@@ -14,15 +42,17 @@ class Room(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    participants = models.ManyToManyField(User, related_name='participants', blank=True)
+    participants = models.ManyToManyField(
+        User, related_name='participants', blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ["-updated", "-created"]
+        ordering = ['-updated', '-created']
 
     def __str__(self):
         return self.name
+
 
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -32,8 +62,7 @@ class Message(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ["-updated", "-created"]
-
+        ordering = ['-updated', '-created']
 
     def __str__(self):
-        return self.body[:50]
+        return self.body[0:50]
